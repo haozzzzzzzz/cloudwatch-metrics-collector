727efb13b6ab8f1ace1bb196ae88af1b3f98135b
luo hao <990747088@qq.com>
2018-08-20 13:37:34 +0800

    add prometheus collector

--- collector/prometheus.go
+++ collector/prometheus.go
@@ -1,7 +1,16 @@
 package collector
 
+import (
+	"github.com/haozzzzzzzz/cloudwatch-metrics-collector/metric"
+)
+
 // prometheus metrics collector
 type PrometheusMetrics struct {
-	Target string
+	Target          string
 	NamespacePrefix string // 匹配namespace前缀
+}
+
+func (m *PrometheusMetrics) Pull() (metrics []*metric.Metric, err error) {
+	metrics = make([]*metric.Metric, 0)
+	return
 }
